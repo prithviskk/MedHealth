b4714f7a7fd27e4b2a62bc5d94b35364fbce2d4e
Prithvi <prithviskk02@gmail.com>
2025-04-08 21:53:35 +0530

    updated env

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin:["http://localhost:5173", "https://medhealth.onrender.com/"],
         credentials:true,
     })
 )
