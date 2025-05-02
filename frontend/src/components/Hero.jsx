37c880e091d7a630ec2b1c00ca519aedb7ffd6c6
Prithvi <prithviskk02@gmail.com>
2025-05-02 22:38:21 +0530

    updated hero banner

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import image1 from '../assets/pngtree-team-or-group-of-doctors-and-nurses-students-png-image_10786107.png';
+import image1 from '../assets/doctors.png';
 import { useNavigate } from 'react-router-dom';
 
 const Hero = ({ toggleForm }) => {
